664a1a2b341df52f60c0dd89f5c4623b3c37694c
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-04-23 22:10:14 -0500

    added separate divs for each choice option

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 
 var questionEl = document.querySelector(".question");
-var choicesEl = document.querySelector(".choices");
+var choice1El = document.querySelector(".choice1");
+var choice2El = document.querySelector(".choice2");
+var choice3El = document.querySelector(".choice3");
+var choice4El = document.querySelector(".choice4");
 var buttonEl = document.querySelector(".button");
 var introductionEl = document.querySelector(".introduction");
 var timerEl = document.querySelector(".timer");
@@ -20,6 +23,11 @@ var choices = ["1. dog", "2. cat", "3. duck", "4. horse",
                 "1. apple", "2. orange", "3. blue", "4. fish",
                 "1. boom", "2. clap", "3. zing", "4. slap"];
 
+// Correct Answer array
+var correctAnswer = ["1. dog",
+                        "4. fish",
+                        "3. zing"];
+
 // Question Function which displays question, choices, and button for next
 function codeQuestion() {
     // Question displayed
@@ -29,19 +37,29 @@ function codeQuestion() {
         // 1st Choice displayed
         var makeChoice1 = document.createElement("p");
         makeChoice1.textContent = choices[u];
-        choicesEl.append(makeChoice1);
+        choice1El.append(makeChoice1);
         // 2nd Choice displayed
         var makeChoice2 = document.createElement("p");
         makeChoice2.textContent = choices[u + 1];
-        choicesEl.append(makeChoice2);
+        choice2El.append(makeChoice2);
         // 3rd Choice displayed
         var makeChoice3 = document.createElement("p");
         makeChoice3.textContent = choices[u + 2];
-        choicesEl.append(makeChoice3);
+        choice3El.append(makeChoice3);
         // 4th Choice displayed
         var makeChoice4 = document.createElement("p");
         makeChoice4.textContent = choices[u + 3];
-        choicesEl.append(makeChoice4);
+        choice4El.append(makeChoice4);
+        // Event Listeners on all choices listening for correct/wrong answers
+        makeChoice1.addEventListener("click", function(event) {
+            var userChoice = event.target;
+
+            if (userChoice.matches("p")) {
+                var answerValue = userChoice.getAttribute("data-answer");
+
+                if (answerValue === "correct") {}
+            }
+        })
     // Next Button displayed
     var makeNxtBtn = document.createElement("button");
     makeNxtBtn.textContent = "Next";
@@ -51,10 +69,10 @@ function codeQuestion() {
     makeNxtBtn.addEventListener("click", function() {
     
         questionEl.removeChild(makeQuestion);
-        choicesEl.removeChild(makeChoice1);
-        choicesEl.removeChild(makeChoice2);
-        choicesEl.removeChild(makeChoice3);
-        choicesEl.removeChild(makeChoice4);
+        choice1El.removeChild(makeChoice1);
+        choice2El.removeChild(makeChoice2);
+        choice3El.removeChild(makeChoice3);
+        choice4El.removeChild(makeChoice4);
         buttonEl.removeChild(makeNxtBtn);
     
         i++;
